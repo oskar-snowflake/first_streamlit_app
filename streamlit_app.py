032192cd4ec4c11fce195771c9e8074600dad711
oskar-snowflake <oskar.eriksson@snowflake.com>
2022-11-25 20:28:13 +0100

    Update streamlit_app.py
    Added df under BYOSmoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,5 @@ streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+streamlit.dataframe(my_fruit_list)
