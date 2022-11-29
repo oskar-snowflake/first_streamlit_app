0c67d4484220e4f0abed2c7fba1c33bdfec855e9
oskar-snowflake <oskar.eriksson@snowflake.com>
2022-11-29 11:52:00 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute('SELECT CURRENT_ROLE()')
+my_cur.execute('USE ROLE PC_RIVERY_ROLE')
+my_cur.execute('SELECT CURRENT_ROLE()
 my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
