51679fca9241e0cdc07feff5975283c3149c3f1e
oskar-snowflake <oskar.eriksson@snowflake.com>
2022-11-29 10:51:43 +0100

    Update streamlit_app.py
    Removed raw json display and parameterized http request

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,8 +22,7 @@ import requests
 
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
 # Normalize the json
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Display normalized json as df
