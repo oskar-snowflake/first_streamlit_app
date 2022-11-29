5e07a2afe69e118cea4073ebc2b42f8afa8d71cf
oskar-snowflake <oskar.eriksson@snowflake.com>
2022-11-29 10:50:25 +0100

    Update streamlit_app.py
    Normalized and displayed json as df

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,3 +24,7 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+# Normalize the json
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display normalized json as df
+streamlit.dataframe(fruityvice_normalized)
